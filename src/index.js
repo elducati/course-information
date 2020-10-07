27624c3c14a9ed5fbc986a8f9a1fbb61b3585377
elducati <geomondi09@gmail.com>
2020-10-07 05:37:11 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <Header />
+      <Content />
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+const Header = (props) => {
+  props = {
+    course: "Half Stack application development"
+  }
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Content = (props) => {
+  props = {
+    part1: 'Fundamentals of React',
+    exercises1: 10,
+    part2: 'Using props to pass data',
+    exercises2: 7,
+    part3: 'State of a component',
+    exercises3: 14
+
+  }
+  return (
+    <div>
+      <p>{props.part1}{props.exercises1}</p>
+      <p>{props.part2}{props.exercises2}</p>
+      <p>{props.part3}{props.exercises3}</p>
+
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
